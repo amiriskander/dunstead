5835618437cc3bd02bb71b8d770e1ebbd588cd96
Amir Iskander <amir.iskander@linkdev.com>
2017-02-08 22:04:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+jarvis
+======
+
+A Symfony project created on February 1, 2017, 12:19 pm.
---
+++ src/AppBundle/Entity/Note.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amir
+ * Date: 2/6/17
+ * Time: 1:31 PM
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="notes")
+ */
+class Note extends Entity
+{
+
+}
