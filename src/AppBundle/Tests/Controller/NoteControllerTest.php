b1679baec3784497091956d9c81cead702a17da1
Amir Iskander <amir.iskander@linkdev.com>
2017-02-19 15:41:34 +0200

    Functional tests for the scenarios notes listing, add/edit/delete

--- src/AppBundle/Tests/Controller/NoteControllerTest.php
+++ src/AppBundle/Tests/Controller/NoteControllerTest.php
@@ -6,6 +6,62 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class NoteControllerTest extends WebTestCase
 {
+
+    public function testNotesListing()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET','/note/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /note/");
+    }
+
+    public function testNotesAddEditDelete()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET','/note/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /note/");
+
+        $crawler = $client->click($crawler->selectLink('Create a new note')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /note/new/");
+
+        $form = $crawler->selectButton('Create')->form(array(
+            'appbundle_note[title]'         =>      'Test',
+            'appbundle_note[description]'   =>      'Test Description',
+            'appbundle_note[userId]'        =>      '1',
+        ));
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /note/ After note creation");
+
+        // Check data in the show view
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+
+        // Edit the entity
+        $crawler = $client->click($crawler->selectLink('Edit')->link());
+
+        $form = $crawler->selectButton('Update')->form(array(
+            'appbundle_note[title]'         => 'Foo',
+            'appbundle_note[description]'   => 'Foo Test Note Description',
+            'appbundle_note[userId]'        =>      '1',
+        ));
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        // Check the element contains an attribute with value equals "Foo"
+        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+
+        // Delete the entity
+        $client->submit($crawler->selectButton('Delete')->form());
+        $crawler = $client->followRedirect();
+
+        // Check the entity has been delete on the list
+        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+
+    }
+
     /*
     public function testCompleteScenario()
     {
@@ -15,11 +71,12 @@ class NoteControllerTest extends WebTestCase
         // Create a new entry in the database
         $crawler = $client->request('GET', '/note/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /note/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        $crawler = $client->click($crawler->selectLink('Create a new note')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_note[field_name]'  => 'Test',
+            'appbundle_note[title]'  => 'Test',
+            'appbundle_note[description]'  => 'Test Note Description',
             // ... other fields to fill
         ));
 
@@ -33,7 +90,8 @@ class NoteControllerTest extends WebTestCase
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_note[field_name]'  => 'Foo',
+            'appbundle_note[title]'  => 'Foo',
+            'appbundle_note[description]'  => 'Foo Test Note Description',
             // ... other fields to fill
         ));
 
@@ -50,6 +108,7 @@ class NoteControllerTest extends WebTestCase
         // Check the entity has been delete on the list
         $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
     }
-
     */
+
+
 }
