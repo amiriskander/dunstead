3200cfe86e49d8eee9cb79b6d1b0807fc8cec787
Amir Iskander <amir.iskander@linkdev.com>
2017-03-29 14:06:14 +0200

    Adding bundles required to create REST API endpoints

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,12 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+
+            // Bundles needed for REST API calls
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+
             new AppBundle\AppBundle(),
         );
 
